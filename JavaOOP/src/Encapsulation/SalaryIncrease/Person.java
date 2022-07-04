5acb6dbc341d686bd3dcaaaff1e4928ec0c6a497
Alexanski13 <alexanderivanov344@gmail.com>
2022-07-04 18:39:55 +0300

    init

---
+++ JavaOOP/src/Encapsulation/SalaryIncrease/Person.java
@@ -0,0 +1,60 @@
+package Encapsulation.SalaryIncrease;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        if (age > 30) {
+            salary = salary + salary * bonus / 100;
+        } else {
+            salary = salary + salary * bonus / 200;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %.1f leva", this.firstName, this.lastName, this.salary);
+    }
+}
